a2e921f9b9f116e5d1c72f29a35592573b6882bd
vyomkeshj <vyomkeshjha@yahoo.com>
2022-06-26 11:36:13 +0200

    req update

--- run_training.sh
+++ run_training.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-conda activate ml_exp
 export GOOGLE_APPLICATION_CREDENTIALS=/home/jha0007/sac.json
-cd mesh_transformer
 #python3 create_finetune_tfrecords.py ./data/sql_data/train_wikisql.txt "training_wikisql" --normalize-with-ftfy --n-repack-epochs 6 --output-dir gs://gpt-j-trainer-sql/data/
 #python3 create_finetune_tfrecords.py ./data/sql_data/validation_wikisql.txt "validation_wikisql" --normalize-with-ftfy --output-dir gs://gpt-j-trainer-sql/data/
-python3 device_train.py --config=./configs/combined_bsize16.json --tune-model-path=gs://gpt-j-trainer-sql/step_383500/
+python3 ./device_train.py --config=./configs/combined_bsize16.json --tune-model-path=gs://gpt-j-trainer-sql/step_383500/
 
 #python3 ./to_hf_weights.py --input-ckpt gs://gpt-j-trainer-sql/mesh_jax_pile_6B_rotary/step_104 --config ./configs/spider.json --output-path ./hf_sql_spider --cpu
 
