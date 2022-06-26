e2e8df2a2ff47c4e58e4141aa51a8cc688940548
vyomkeshj <vyomkeshjha@yahoo.com>
2022-06-26 12:05:20 +0200

    req update

--- run_training.sh
+++ run_training.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 export GOOGLE_APPLICATION_CREDENTIALS=/home/jha0007/sac.json
+
+# To convert the data to tfrecords and upload to bucket
 #python3 create_finetune_tfrecords.py ./data/sql_data/train_wikisql.txt "training_wikisql" --normalize-with-ftfy --n-repack-epochs 6 --output-dir gs://gpt-j-trainer-sql/data/
 #python3 create_finetune_tfrecords.py ./data/sql_data/validation_wikisql.txt "validation_wikisql" --normalize-with-ftfy --output-dir gs://gpt-j-trainer-sql/data/
+
+# To run training with a config and a previous saved model checkpoint
 python3 ./device_train.py --config=./configs/combined_bsize16.json --tune-model-path=gs://gpt-j-trainer-sql/step_383500/
 
+# To convert the model to pytorch weights for hugging face inference
 #python3 ./to_hf_weights.py --input-ckpt gs://gpt-j-trainer-sql/mesh_jax_pile_6B_rotary/step_104 --config ./configs/spider.json --output-path ./hf_sql_spider --cpu
 
+# To convert the model to fp16 for tpu inference
+#python3 slim_model.py --config=./configs/combined.json --ckpt-step=201 --f16
