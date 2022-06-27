b1a1e4e27b4218b61bba1ad00703e17704c766a8
vyomkeshj <vyomkeshjha@yahoo.com>
2022-06-27 19:38:15 +0200

    req update

--- run_training.sh
+++ run_training.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 export GOOGLE_APPLICATION_CREDENTIALS=/home/jha0007/sac.json
+CONFIG_FILE="./configs/combined_bsize8.json"
 
 # To convert the data to tfrecords and upload to bucket
 python3 create_finetune_tfrecords.py ./data/sql_data/combined.txt "training_combined_2" --normalize-with-ftfy --n-repack-epochs 3 --seed 16 --verbose --output-dir gs://gpt-j-trainer-sql/data/
 #python3 create_finetune_tfrecords.py ./data/sql_data/validation_wikisql.txt "validation_wikisql" --normalize-with-ftfy --output-dir gs://gpt-j-trainer-sql/data/
 
 # To run training with a config and a previous saved model checkpoint
-python3 ./device_train.py --config=./configs/combined_bsize8.json --tune-model-path=gs://gpt-j-trainer-sql/step_383500/
+python3 ./device_train.py --config=$CONFIG_FILE --tune-model-path=gs://gpt-j-trainer-sql/step_383500/
+
+# you can find the correct one in the cloud bucket at gpt-j-trainer-sql/
+CHECKPOINT_TO_SAVE=1438
 
 # To convert the model to fp16 for tpu inference, select a saved version
-python3 slim_model.py --config=./configs/combined_bsize8.json --ckpt-step=1438 --f16
+python3 slim_model.py --config=$CONFIG_FILE --ckpt-step=$CHECKPOINT_TO_SAVE --f16
 
 # To convert the model to pytorch weights for hugging face inference
-python3 ./to_hf_weights.py --input-ckpt gs://gpt-j-trainer-sql/sql_cleaned_slim_f16/step_1438 --config ./configs/combined_bsize8.json --output-path gs://gpt-j-trainer-sql/hf_cleaned_slim_f16/ --cpu
+python3 ./to_hf_weights.py --input-ckpt "gs://gpt-j-trainer-sql/sql_cleaned_slim_f16/step_""$CHECKPOINT_TO_SAVE" --config $CONFIG_FILE --output-path "gs://gpt-j-trainer-sql/hf_"$CHECKPOINT_TO_SAVE/ --cpu
