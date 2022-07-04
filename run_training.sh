aace52ded35a081f302ee0c632b249740a9fca5b
vyomkeshj <vyomkeshjha@yahoo.com>
2022-07-04 13:05:00 +0200

    req update

--- run_training.sh
+++ run_training.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 export GOOGLE_APPLICATION_CREDENTIALS=/home/jha0007/sac.json
-CONFIG_FILE="./configs/combined_bsize8.json"
+CONFIG_FILE="./configs/wikisql_matchformat.json"
 
 # To convert the data to tfrecords and upload to bucket
-#python3 create_finetune_tfrecords.py ./data/sql_data/spider_reformat.txt "spider_reformat" --normalize-with-ftfy --n-repack-epochs 1 --seed 16 --verbose --output-dir gs://gpt-j-trainer-sql/data/
-#python3 create_finetune_tfrecords.py ./data/sql_data/validation_wikisql.txt "validation_wikisql" --normalize-with-ftfy --output-dir gs://gpt-j-trainer-sql/data/
+python3 create_finetune_tfrecords.py ./data/sql_data/train_wsql_mf.txt "wsql_reformat_train" --normalize-with-ftfy --n-repack-epochs 1 --seed 16 --verbose --output-dir gs://gpt-j-trainer-sql/data/
+python3 create_finetune_tfrecords.py ./data/sql_data/test_wsql_mf.txt "wsql_reformat_val" --normalize-with-ftfy --output-dir gs://gpt-j-trainer-sql/data/
 
 # To run training with a config and a previous saved model checkpoint
-python3 ./device_train.py --config=$CONFIG_FILE --tune-model-path=gs://gpt-j-trainer-sql/step_383500/
+#python3 ./device_train.py --config=$CONFIG_FILE --tune-model-path=gs://gpt-j-trainer-sql/step_383500/
 
 # To make this model available for testing
 
 # you can find the correct one in the cloud bucket at gpt-j-trainer-sql/
 #CHECKPOINT_TO_SAVE=1438
 
-python3 slim_model.py --config="./configs/combined_bsize8.json" --f16
-
-cp -r ./slim_model/ ./home/jha0007/api-gpt/slim_model/
-nohup python3 ./home/jha0007/api-gpt/serve.py &
+#python3 slim_model.py --config="./configs/combined_bsize8.json" --f16
+#
+#cp -r ./slim_model/ ./home/jha0007/api-gpt/slim_model/
+#nohup python3 ./home/jha0007/api-gpt/serve.py &
 
 
 # To convert the model to fp16 for tpu inference, select a saved version
