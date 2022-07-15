f3c26f3aee2edcccf2aaa4ec9aa352b25f562769
Anmol Srivastava <anmol150srivastava@gmail.com>
2022-07-16 04:19:10 +0530

    filter unicode

--- clean.py
+++ clean.py
@@ -12,8 +12,11 @@ with open(file_path, 'r') as json_file:
     result = []
     for row in track(json_list, description="Processing..."):
         items = sqlparse.parse(row)
-        if (len(items) > 0) and ("select" in row.lower()) and ("from" in row.lower()):
-            res = row.replace("\\n", " ").replace("\\t", " ").replace("\\r", " ").replace("\n", " ").replace("\t", " ").replace("\r", " ").replace("\\\/", " ").replace("\\//", " ").replace("\/", " ")
+        if "\\u" in row or "\u" in row or "\\N" in row or "\N" in row:
+            continue
+        # if (len(items) > 0) and ("select" in row.lower()) and ("from" in row.lower()):
+        if len(items) > 0:
+            res = row.replace("\\n", " ").replace("\\t", " ").replace("\\r", " ").replace("\n", " ").replace("\t", " ").replace("\r", " ").replace("\/", "/").replace("\x", "x")
             res = re.sub('\s+', ' ', res)
             result.append(res)
     res_file_path = f"{file_path.split('.')[0]}_cleaned.{file_path.split('.')[1]}"
